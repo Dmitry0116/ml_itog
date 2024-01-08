2604d7139a4d5fbcb353bee9afc06639785253e7
Dmitry0116 <155762834+Dmitry0116@users.noreply.github.com>
2024-01-08 17:32:16 +0500

    Добавлены файлы

---
+++ test_main.py
@@ -0,0 +1,57 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_predict_joy():
+    response = client.post("/predict/",
+                           json={"text": "Будьте благополучны и радостны, и передайте мне на расстоянии, чтобы я знал, что с Вами все хорошо."})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'joy'
+
+
+def test_predict_no_emotion():
+    response = client.post("/predict/",
+                           json={"text": "Данный фрагмент текста не содержит абсолютно никаких эмоций"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'no_emotion'
+
+
+def test_predict_sadness():
+    response = client.post("/predict/",
+                           json={"text": "Грусть-тоска меня съедает"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'sadness'
+
+
+def test_predict_surprise():
+    response = client.post("/predict/",
+                           json={"text": "Нифига себе, неужели так тоже бывает!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'surprise'
+    
+
+def test_predict_fear():
+    response = client.post("/predict/",
+                           json={"text": "Как-то стрёмно, давай свалим отсюда?"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'fear'
+
+def test_predict_anger():
+    response = client.post("/predict/",
+                           json={"text": "Бесишь меня, гад"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'anger' 
