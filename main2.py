89cbb996d7d20f50d6be797799f5cc37397fad14
Dmitry0116 <155762834+Dmitry0116@users.noreply.github.com>
2024-01-08 18:36:28 +0500

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+st.title('Приложение определяет эмоции в предложениях на русском языке.')
+
+form = st.form(key='sentiment-form')
+user_input = form.text_area('Введите Ваш текст')
+submit = form.form_submit_button('Определить')
+
+if submit:
+    clf = pipeline(
+        task = 'text-classification', 
+        model = 'cointegrated/rubert-tiny2-cedr-emotion-detection')
+     
+    result = clf(user_input)
+
+
+    for res in result:
+       label = res['label']
+       score = res['score']
+       st.success(f'{label} sentiment (score: {score})')
+ 
+    
+
+        
+class Item(BaseModel):
+    text: str
+
+app = FastAPI()
+clf = pipeline(
+    task = 'text-classification', 
+    model = 'cointegrated/rubert-tiny2-cedr-emotion-detection')
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict/")
+def predict(item: Item):
+    """Распознование эмоций в тексте на русском языке"""
+    return clf(item.text)
