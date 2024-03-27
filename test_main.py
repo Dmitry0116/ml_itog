072bd9c0283bceef168db886df21de032666dd65
Дмитрий Кудлай <155762834+Dmitry0116@users.noreply.github.com>
2024-03-27 23:21:42 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -11,6 +11,7 @@ def test_read_main():
 
 
 def test_predict_joy():
+    print ("прочитай меня")
     response = client.post("/predict/",
                            json={"text": "Будьте благополучны и радостны, и передайте мне на расстоянии, чтобы я знал, что с Вами все хорошо."})
     json_data = response.json()
